93d3b675873291f42be229238769b786eabc29a5
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-12-27 14:05:36 -0300

    add prometheus metrics server

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"os"
 	"log"
+    "net/http"
 
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+func run_metrics_server() {
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
+
 func main() {
+
+	go run_metrics_server()
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
 	if err != nil {
 		log.Panic(err)
@@ -23,7 +33,7 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
+		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "miau")
